c6c635394f25d0f1832744cf8ff789a3dd0da558
David Fisher <davidf@thelevelup.com>
2016-03-27 16:29:42 -0400

    testing FPS on the Pi

--- lights.py
+++ lights.py
@@ -9,10 +9,11 @@ from collections import deque
 from PIL import Image
 from io import BytesIO
 import PIL
+import time
 
 
 HEIGHT = 240        # Number of LEDS (60 per meter)
-WIDTH = 4           # Number of LED strips
+WIDTH = 6           # Number of LED strips
 DATAPIN   = 10      # Data Pin for SPI. Probably needs a 3.3v to 5v level stepper
 CLOCKPIN  = 11      # Clock Pin for SPI. Probably needs a 3.3v to 5v level stepper
 BLUR_AMOUNT = 29    # Blur amount. More is better for accuracy.
@@ -21,12 +22,16 @@ OFFSET = 0
 
 # Performs a strong GaussianBlur
 # image:Image -> Image
+# takes 20fps
+# @profile
 def blur(image):
-    return cv2.GaussianBlur(image, (BLUR_AMOUNT, BLUR_AMOUNT),25)
+    # cv2.GaussianBlur(image, (BLUR_AMOUNT, BLUR_AMOUNT),25) #4873963
+    return cv2.blur(image,(BLUR_AMOUNT,BLUR_AMOUNT)) #865923
 
 
 # Converts from BGR to HSV colorspace
 # image:Image -> Image
+# @profile
 def convert_color(image):
     return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 
@@ -34,6 +39,7 @@ def convert_color(image):
 # Displays an HSV image on screen for debugging
 # Not as needed with cv2.imshow
 # Image -> False
+# @profile
 def display_hsv_image_array(hsv_image):
     rgb_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2RGB)
     im = PIL.Image.fromarray(rgb_image)
@@ -44,6 +50,9 @@ def display_hsv_image_array(hsv_image):
 
 # Processes one image frame
 # frame:Image, bitmap:Deque, strip:Adafruit_DotStar -> False
+# 80% of time is in here
+# 10.0365 s
+# @profile
 def flow(frame, bitmap, strip):
     solution = process_frame(frame)
     bitmap.appendleft(solution)
@@ -53,10 +62,14 @@ def flow(frame, bitmap, strip):
 
 # Finds the index of the maximum saturated pixel
 # hsv_image:Image -> (hue:Int, sat:Int, lum:Int)
+# 0.309177 s
+# @profile
 def find_dominant_color(hsv_image):
     hue_channel = hsv_image[:,:,0]
-    sat_channel = hsv_image[:,:,1]
+    _, sat_channel = cv2.threshold(hsv_image[:,:,1], 80, 256, cv2.THRESH_TOZERO)
     lum_channel = hsv_image[:,:,2]
+
+    # What's happening is that then the max saturation becomes meh, and becomes a blah one for all
 
     max_index = unravel_index(sat_channel.argmax(), sat_channel.shape)
     hue = hue_channel[max_index]
@@ -68,6 +81,7 @@ def find_dominant_color(hsv_image):
 
 # Creates an empty bitmap
 # () -> image:Deque
+# @profile
 def initialize_empty_bitmap():
     bitmap = deque(maxlen=WIDTH*HEIGHT)
     empty_element = 0, 0, 0
@@ -81,9 +95,12 @@ def initialize_empty_bitmap():
 
 # Finds the max hue/saturation across the image
 # frame:Image -> Deque
+# 5.79235 s
+# @profile
 def process_frame(frame):
 
-    ready_frame = blur(convert_color(shrink(frame)))
+    ready_frame = blur(convert_color(shrink(frame))) # 90% of the time here is spent in this
+    cv2.imshow('cameraPreview', cv2.cvtColor(ready_frame, cv2.COLOR_HSV2RGB))
     bright_points = deque()
     (height, width, _) = ready_frame.shape
     slice_size = width / WIDTH
@@ -99,6 +116,7 @@ def process_frame(frame):
 
 
 # Zig-zags around for the led strips in rows.
+# @profile
 def wrap_and_display_to_leds(image, strip):
     for led_strip_index in xrange(WIDTH):
         for led_length_index in xrange(HEIGHT):
@@ -112,13 +130,18 @@ def wrap_and_display_to_leds(image, strip):
 
 
 # Renders to LEDs or screen
+# 2.95871 s
+# @profile
 def render_bitmap(bitmap, strip):
-    np_bitmap = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
-    for row_idx, row in enumerate(bitmap):
-        for column_idx, column in enumerate(row):
-            np_bitmap[row_idx][column_idx] = bitmap[row_idx][column_idx]
+    # 70% of this is spent in the following. Can I get around that?
+    # Surely some way to turn those queues around. Or maybe just array them to begin with?
+    np_bitmap = np.array(bitmap, np.uint8) # A faster than below. Accurate?
+    # np_bitmap = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
+    # for row_idx, row in enumerate(bitmap):
+    #     for column_idx, column in enumerate(row):
+    #         np_bitmap[row_idx][column_idx] = bitmap[row_idx][column_idx]
 
-    rgb_image = cv2.cvtColor(np_bitmap, cv2.COLOR_HSV2RGB)
+    rgb_image = cv2.cvtColor(np_bitmap, cv2.COLOR_HSV2RGB) # This is 25% of the time
     b,g,r = cv2.split(rgb_image)
     img = cv2.merge((g,r,b))
 
@@ -128,19 +151,28 @@ def render_bitmap(bitmap, strip):
         cv2.imshow('preview', enlarge(rgb_image))
 
 # Enlarges the image. Used for drawing pixels on computer for debugging
+# @profile
 def enlarge(image):
     return cv2.resize(image,None,fx=40, fy=3, interpolation = cv2.INTER_AREA)
 
 
 # Shrinks image to 30% of original size. Makes faster and more accurate
 # image:Image -> Image
+# 4.61985 s Lots of time here. Faster way? Different interpolation?
+# @profile
 def shrink(image):
-    return cv2.resize(image,None,fx=0.3, fy=0.3, interpolation = cv2.INTER_AREA)
+    # cv2.resize(image,None,fx=0.3, fy=0.3, interpolation = cv2.INTER_LINEAR) # 1000869
+    # cv2.resize(image,None,fx=0.3, fy=0.3, interpolation = cv2.INTER_AREA) # 4782976
+    # cv2.resize(image,None,fx=0.3, fy=0.3, interpolation = cv2.INTER_CUBIC) # 2115175
+    # cv2.resize(image,None,fx=0.3, fy=0.3, interpolation = cv2.INTER_LANCZOS4) # 5460249
+    return cv2.resize(image,None,fx=0.3, fy=0.3, interpolation = cv2.INTER_NEAREST) #  359332 (way fastest)
+
 
 
 # This is the main function
 # It is kinda messy because it sets up alternate paths for various modes
 # (args) -> Boolean
+# @profile
 def main(argv):
     global MODE
     global INPUT
@@ -173,6 +205,7 @@ def main(argv):
             cv2.namedWindow('cameraPreview', cv2.WINDOW_NORMAL)
         vc = cv2.VideoCapture(0)
         if vc.isOpened():
+            vc.set(15, -10)
             vc.set(3,200) # These aren't accurate, but help
             vc.set(4,100)
             rval, frame = vc.read()
@@ -181,12 +214,15 @@ def main(argv):
 
         while rval:
             rval, frame = vc.read()
-            if MODE == 'debug':
-                cv2.imshow('cameraPreview', frame)
+            # if MODE == 'debug':
+                # cv2.imshow('cameraPreview', frame)
+            start = time.time()
             flow(frame, bitmap, strip)
             key = cv2.waitKey(15)
             if key == 27: # exit on ESC
                 break
+            end = time.time()
+            print(end - start)
 
     # If you're using a static image for debugging
     if INPUT == 'image':
@@ -196,11 +232,19 @@ def main(argv):
             frame = cv2.imread('bars.jpg')
         rval = True
 
+        # while True:
+        # For 1000 frames
+        start = time.time()
         while True:
             flow(frame, bitmap, strip)
             key = cv2.waitKey(15)
             if key == 27: # exit on ESC
                 break
+        end = time.time()
+        fps = 1000 / (end - start)
+        print('fps:', fps)
+
+
 
     # If you're using a pre-recorded video for debugging set it here
     if INPUT == 'video':

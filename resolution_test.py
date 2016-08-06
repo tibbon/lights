9d5207bbfaa5f9f7b3b5249e8d195e70381d740a
David Fisher <tibbon@gmail.com>
2016-08-06 17:09:17 -0400

    Trying height

--- resolution_test.py
+++ resolution_test.py
@@ -6,8 +6,8 @@ def main(argv):
   camera= cv2.VideoCapture(0)
 
   for i in range(120, 1920):
-    result = camera.set(3, i)
-    print(result, ": ", i)
+    result = camera.set(4, i)
+    print(result, i)
 
 if __name__ == '__main__':
    main(sys.argv[1:])

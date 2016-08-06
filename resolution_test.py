d3978e9ebf74a1a8cd1c2e175bd75216d117b2c5
David Fisher <tibbon@gmail.com>
2016-08-06 17:34:55 -0400

    typo

--- resolution_test.py
+++ resolution_test.py
@@ -5,7 +5,7 @@ import sys
 def main(argv):
   print('OpenCV version: ', cv2.__version__)
   camera= cv2.VideoCapture(0)
-  print("format ", camera.set(CAP_PROP_CONVERT_RGB, 0))
+  print("format ", camera.set(cv2.CAP_PROP_CONVERT_RGB, 0))
   camera.set(cv2.CAP_PROP_AUTOFOCUS, False)
   print("FPS: ", int(camera.get(cv2.CAP_PROP_FPS)))
   print(camera.get(cv2.CAP_PROP_CONVERT_RGB))
